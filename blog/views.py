ac0f1ae6dead3b2992ff4ef524a9f590d4645115
raemaxxi <reema.aljuaid@gmail.com>
2020-08-30 10:49:53 +0300

    l

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
+def homepage(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    return render(request, 'blog/homepage.html', {'posts' : posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,3 +37,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('homepage')
